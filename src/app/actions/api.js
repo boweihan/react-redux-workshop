d7f213d0859176f0254ce975ad82e70bac37515d
Filip Stankovic <filips@gmail.com>
2018-09-18 01:46:20 -0400

    Add thunk request example

--- src/app/actions/api.js
+++ src/app/actions/api.js
@@ -9,20 +9,12 @@ const HEADERS_BASE = {
 
 const API_URL_BASE = 'http://localhost:8000/';
 
-export const fetchBooks = () => {
-  return {
-    [RSAA]: {
-      endpoint: API_URL_BASE + 'books',
-      method: 'GET',
-      types: [
-        types.BOOKS_FETCH_REQUEST,
-        types.BOOKS_FETCH_SUCCESS,
-        types.BOOKS_FETCH_FAILURE,
-      ],
-      headers: HEADERS_BASE,
-    },
-  };
-};
+/*
+* Redux API Middleware (https://github.com/agraboso/redux-api-middleware)
+* will intercept any action that contains the RSAA key, and instead dispatch
+* some of the actions defined in the types property below, based on how
+* the request resolves. It uses the new JavaScript Fetch API internally.
+*/
 
 export const fetchUsers = () => {
   return {
@@ -38,3 +30,67 @@ export const fetchUsers = () => {
     },
   };
 };
+
+/*
+* This is more or less functionally equivalent with the commented out
+* redux-api-middleware fetchBooks action defined below. There are many
+* different ways to handle async actions in redux. redux-api-middleware is
+* a nice abstraction (IMO ;) for AJAX requests, because they always have
+* one initial action and two possible outcomes, among other similarities.
+*
+* Here, we are directly calling Fetch, while returning a function from our
+* action creator. This returned function is intercepted by thunk middleware
+* and called with an action dispatcher, allowing us to dispatch multiple
+* actions in one method. The way you decide to control dispatching these
+* actions (Promise, Observable, timeout, etc.) is completely up to you.
+*/
+
+export const fetchBooks = () => dispatch => {
+  // Generic error handling for Fetch requests.
+  function handleErrors(response) {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response;
+  }
+
+  fetch(API_URL_BASE + 'books')
+    .then(handleErrors)
+    .then(response => response.json())
+    .then(jsonResponse =>
+      dispatch({
+        type: types.BOOKS_FETCH_SUCCESS,
+        payload: jsonResponse,
+      }),
+    )
+    // The error signature here is not exactly the same as RSAA, which has
+    // specific error handling semantics. If you're interested, you can
+    // read more about their error handling philosohpy and implementation
+    // here: https://github.com/redux-utilities/flux-standard-action
+    // This is not important for this example.
+    .catch(error =>
+      dispatch({
+        type: types.BOOKS_FETCH_FAILURE,
+        error: error,
+      }),
+    );
+
+  return dispatch({
+    type: types.BOOKS_FETCH_REQUEST,
+  });
+};
+
+// export const fetchBooks = () => {
+//   return {
+//     [RSAA]: {
+//       endpoint: API_URL_BASE + 'books',
+//       method: 'GET',
+//       types: [
+//         types.BOOKS_FETCH_REQUEST,
+//         types.BOOKS_FETCH_SUCCESS,
+//         types.BOOKS_FETCH_FAILURE,
+//       ],
+//       headers: HEADERS_BASE,
+//     },
+//   };
+// };
